1ef48d9db0580dbee3ebb0338e84a3546f2a563f
sabbir <sabbir.hossain.se62@gmail.com>
2025-01-30 22:42:37 +0600

    another solution for random number

--- Random Topics/randomNumber.js
+++ Random Topics/randomNumber.js
@@ -1,7 +1,14 @@
 // print 1-6 random number for ludu game
 
-const randomNumber = () => {
-    console.log(Math.floor(Math.random() * 6) + 1);
+// Solution 1
+// const randomNumber = () => {
+//     console.log(Math.floor(Math.random() * 6) + 1);
+// }
+// randomNumber();
+
+// Solution 2
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-randomNumber();
+console.log(randomNumber(1, 6));
