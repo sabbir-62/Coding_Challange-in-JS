7d29a89327d43a8e8a3aba131064c97a4bd42132
sabbir hossain <sabbir.hossain.se62@gmail.com>
2025-02-03 23:59:54 +0600

    add

---
+++ Random Topics/array_traversing.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Array Traversing
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// Access specific element
+
+let elementIndex = 4;
+console.log(arr[elementIndex])
