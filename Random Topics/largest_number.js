0dbb60bb0fe9ffe246fcb9afe0ba60e60c20ee55
sabbir hossain <sabbir.hossain.se62@gmail.com>
2025-02-04 00:40:05 +0600

    2nd largest number

--- Random Topics/largest_number.js
+++ Random Topics/largest_number.js
@@ -3,4 +3,9 @@
 // Find largest number
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(Math.max(...arr));
+// console.log(Math.max(...arr));
+
+// 2nd largest Number
+
+let sortedArray = arr.sort((a,b) => b-a);
+console.log(sortedArray[1])
