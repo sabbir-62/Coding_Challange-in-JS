7a317faecb0cc248a322b9244fbc284f8bcb5148
sabbir hossain <sabbir.hossain.se62@gmail.com>
2025-03-10 23:16:38 +0600

    insert number in specific position

--- Random Topics/insert_number.js
+++ Random Topics/insert_number.js
@@ -1,4 +1,17 @@
 const prompt = require('prompt-sync')();
 
-const number = prompt('Enter the number: ');
-console.log('number', number);
+const number = prompt('Enter the number: ').split(' ').map(Number)
+const position = Number(prompt('Enter the position: '));
+const value = Number(prompt('Enter the value: '));
+
+console.log('Before:', number);
+
+// Shifting elements to the right
+for (let i = number.length; i > position - 1; i--) {
+    number[i] = number[i - 1]; // Shift elements right
+}
+
+// Insert the new value at the specified position
+number[position] = value;
+
+console.log('After:', number);
